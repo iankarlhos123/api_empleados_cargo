5a407cd61eb0ee14f1a396cb17a5e06b31e72ad8
iankarlhos <iankarlhossolanogarcia@gmail.com>
2026-06-09 09:20:30 -0500

    los test

--- tests/Feature/CargoTest.php
+++ tests/Feature/CargoTest.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Models\Cargo;       
-use App\Models\Empleado; 
+use App\Models\Cargo;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
@@ -20,27 +19,57 @@ test('puede crear un cargo', function () {
     ]);
 });
 
-test('puede mostrar un empleado', function () {
+test('puede mostrar un cargo', function () {
 
     $cargo = Cargo::create([
         'nombre_cargo' => 'Programador',
         'descripcion' => 'Desarrolla software',
     ]);
 
-    $empleado = Empleado::create([
-        'nombres' => 'lucho',
-        'apellidos' => 'Diaz',
-        'estado' => true,
-        'id_cargo' => $cargo->id_cargo,
-    ]);
-
-    $response = $this->getJson("/api/empleados/{$empleado->id_empleado}");
+    $response = $this->getJson("/api/cargos/{$cargo->id_cargo}");
 
     $response->assertStatus(200);
 
     $response->assertJson([
-        'id_empleado' => $empleado->id_empleado,
-        'nombres' => 'lucho',
+        'id_cargo' => $cargo->id_cargo,
+        'nombre_cargo' => 'Programador',
     ]);
 });
 
+test('puede actualizar un cargo', function () {
+
+    $cargo = Cargo::create([
+        'nombre_cargo' => 'Programador',
+        'descripcion' => 'Desarrolla software',
+    ]);
+
+    $response = $this->putJson("/api/cargos/{$cargo->id_cargo}", [
+        'nombre_cargo' => 'Analista',
+        'descripcion' => 'Analiza sistemas',
+    ]);
+
+    $response->assertStatus(200);
+
+    $this->assertDatabaseHas('cargos', [
+        'id_cargo' => $cargo->id_cargo,
+        'nombre_cargo' => 'Analista',
+    ]);
+});
+
+test('puede eliminar un cargo', function () {
+
+    $cargo = Cargo::create([
+        'nombre_cargo' => 'Programador',
+        'descripcion' => 'Desarrolla software',
+    ]);
+
+    $response = $this->deleteJson(
+        "/api/cargos/{$cargo->id_cargo}"
+    );
+
+    $response->assertStatus(200);
+
+    $this->assertDatabaseMissing('cargos', [
+        'id_cargo' => $cargo->id_cargo,
+    ]);
+});
--- tests/Feature/FuncionCargoTest.php
+++ tests/Feature/FuncionCargoTest.php
@@ -48,3 +48,54 @@ test('puede mostrar una funcion de cargo', function () {
         'descripcion_funcion' => 'Desarrollar aplicaciones',
     ]);
 });
+
+test('puede actualizar una funcion de cargo', function () {
+
+    $cargo = Cargo::create([
+        'nombre_cargo' => 'Programador',
+        'descripcion' => 'Desarrolla software',
+    ]);
+
+    $funcion = FuncionCargo::create([
+        'descripcion_funcion' => 'Desarrollar aplicaciones',
+        'estado' => true,
+        'id_cargo' => $cargo->id_cargo,
+    ]);
+
+    $response = $this->putJson("/api/funciones-cargo/{$funcion->id_funcion}", [
+        'descripcion_funcion' => 'Desarrollar sistemas',
+        'estado' => false,
+        'id_cargo' => $cargo->id_cargo,
+    ]);
+
+    $response->assertStatus(200);
+
+    $this->assertDatabaseHas('funciones_cargo', [
+        'id_funcion' => $funcion->id_funcion,
+        'descripcion_funcion' => 'Desarrollar sistemas',
+    ]);
+});
+
+test('puede eliminar una funcion de cargo', function () {
+
+    $cargo = Cargo::create([
+        'nombre_cargo' => 'Programador',
+        'descripcion' => 'Desarrolla software',
+    ]);
+
+    $funcion = FuncionCargo::create([
+        'descripcion_funcion' => 'Desarrollar aplicaciones',
+        'estado' => true,
+        'id_cargo' => $cargo->id_cargo,
+    ]);
+
+    $response = $this->deleteJson(
+        "/api/funciones-cargo/{$funcion->id_funcion}"
+    );
+
+    $response->assertStatus(200);
+
+    $this->assertDatabaseMissing('funciones_cargo', [
+        'id_funcion' => $funcion->id_funcion,
+    ]);
+});
