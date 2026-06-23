8d25dcaa27db192e970cb1209c72812b878de107
iankarlhos <iankarlhossolanogarcia@gmail.com>
2026-06-22 20:26:30 -0500

    Reorganizar rutas de autenticación y lectura pública en api.php

--- routes/api.php
+++ routes/api.php
@@ -6,24 +6,25 @@ use App\Http\Controllers\CargoController;
 use App\Http\Controllers\EmpleadoController;
 use App\Http\Controllers\FuncionCargoController;
 
-// Autenticación
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login',    [AuthController::class, 'login']);
-Route::post('logout',   [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// Lectura pública
-Route::get('empleados',                                  [EmpleadoController::class, 'index']);
-Route::get('empleados/{empleado}',                       [EmpleadoController::class, 'show']);
-
-Route::get('cargos',                                     [CargoController::class, 'index']);
-Route::get('cargos/{cargo}',                             [CargoController::class, 'show']);
-Route::get('cargos/{cargo}/funciones',                   [FuncionCargoController::class, 'porCargo']);
-
-Route::get('funciones-cargo',                            [FuncionCargoController::class, 'index']);
-Route::get('funciones-cargo/{funcionCargo:id_funcion}',  [FuncionCargoController::class, 'show']);
+    // Autenticación Lectura pública
+   Route::post('register', [AuthController::class, 'register']);
+   Route::post('login',    [AuthController::class, 'login']);
+   Route::post('logout',   [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // Rutas protegidas 
 Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('empleados',                                  [EmpleadoController::class, 'index']);
+    Route::get('empleados/{empleado}',                       [EmpleadoController::class, 'show']);
+
+    Route::get('cargos',                                     [CargoController::class, 'index']);
+    Route::get('cargos/{cargo}',                             [CargoController::class, 'show']);
+    Route::get('cargos/{cargo}/funciones',                   [FuncionCargoController::class, 'porCargo']);
+
+    Route::get('funciones-cargo',                            [FuncionCargoController::class, 'index']);
+    Route::get('funciones-cargo/{funcionCargo:id_funcion}',  [FuncionCargoController::class, 'show']);
+
 
     Route::post('empleados',              [EmpleadoController::class, 'store']);
     Route::put('empleados/{empleado}',    [EmpleadoController::class, 'update']);
